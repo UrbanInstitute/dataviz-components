6788cfcb467ca1271875bc93656d65fdd7fabb1f
mitchthorson <mitchell@mitchellthorson.com>
2023-11-30 00:23:42 -0500

    Update storybook theme

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,20 @@
 /** @type { import('@storybook/sveltekit').StorybookConfig } */
 const config = {
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)"],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
-		"@storybook/addon-interactions"
+		"@storybook/addon-interactions",
+		"@storybook/addon-svelte-csf"
 	],
 	framework: {
 		name: "@storybook/sveltekit",
 		options: {}
 	},
+  core: { disableTelemetry: true },
 	docs: {
 		autodocs: "tag"
-	}
+	},
+  staticDirs: ["../static/"]
 };
 export default config;
