49b917fc39a989fb95fce0f6fcfc702fd97d4bf6
mitchthorson <mitchell@mitchellthorson.com>
2023-12-21 12:44:30 -0500

    Add text block

---
+++ src/lib/TextBlock/TextBlock.stories.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+	import TextBlock from "./TextBlock.svelte";
+
+	export const meta = {
+		title: "Components/TextBlock",
+		description: "A basic text block",
+		component: TextBlock,
+		tags: ["autodocs"],
+	};
+</script>
+
+<script>
+	import { Story, Template } from "@storybook/addon-svelte-csf";
+
+</script>
+
+<Template let:args>
+	<TextBlock {...args} />
+</Template>
+
+<Story
+	name="Default"
+  args={{
+    content: "Enim id qui labore labore quis ut enim tempor sint quis proident voluptate ex. Duis nisi minim et occaecat do ullamco nisi dolore ipsum proident tempor aute exercitation duis. Proident pariatur consectetur tempor mollit Lorem deserunt. Ut laboris est cillum ut dolore quis consectetur nostrud ut cupidatat enim ea cupidatat ipsum. Voluptate esse labore incididunt sint eiusmod ullamco mollit consequat.",
+  }}
+/>
+
+<Story
+	name="With HTML content"
+  args={{
+    content: "Enim id qui <a href='https://urban.org' target='_blank'>labore labore quis</a> ut enim tempor sint quis proident voluptate ex. Duis nisi minim et occaecat do ullamco nisi dolore ipsum proident tempor aute exercitation duis. Proident pariatur consectetur tempor mollit Lorem deserunt. Ut laboris est cillum ut dolore quis consectetur nostrud ut cupidatat enim ea cupidatat ipsum. Voluptate esse labore incididunt sint eiusmod ullamco mollit consequat.",
+  }}
+/>
---
+++ src/lib/TextBlock/TextBlock.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "$lib/style/app.css";
+  /**
+   * The text to display in the block. Allows HTML content.
+   * @type {string} [textBlock]
+   */
+  export let content;
+</script>
+
+<p class="text-block text-width">{@html content}</p>
+
+<style>
+  p.text-block {
+    font-family: var(--font-family-sans);
+    font-size: var(--font-size-large);
+    font-weight: var(--font-weight-normal);
+    line-height: var(--line-height-normal);
+    color: var(--color-gray-shade-darkest);
+    margin-bottom: var(--spacing-8);
+  }
+
+  @media (min-width: 768px) {
+    p.text-block {
+      font-size: var(--font-size-xl);
+      line-height: var(--line-height-relaxed);
+    }
+  }
+</style>
