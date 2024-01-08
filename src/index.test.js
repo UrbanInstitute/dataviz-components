256fec4772f120100cdccb3645665b31b64e743b
Ben Kates <benekates@gmail.com>
2024-01-08 14:53:19 -0500

    add tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,20 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
+import { dollarFormat, kebabCase } from "$lib/utils/index.js";
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
+describe("sum test", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
+
+describe("kebab-case test", () => {
+  it("converts a string to kebab-case", () => {
+    expect(kebabCase("Hello World")).toBe("hello-world");
+  });
+});
+
+describe("dollar format test", () => {
+  it("converts a number to a dollar-formatted string", () => {
+    expect(dollarFormat(1000000)).toBe("$1,000,000");
+  });
 });
