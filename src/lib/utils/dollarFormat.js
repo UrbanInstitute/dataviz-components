89c056c4069abbb18c63309b5e33410d505b169a
Ben Kates <benekates@gmail.com>
2024-01-08 14:42:30 -0500

    init dollar format

---
+++ src/lib/utils/dollarFormat.js
@@ -0,0 +1,11 @@
+import { format } from "d3-format";
+/**
+ * Formats a number as a dollar amount.
+ *
+ * @param {number} x - The number to format.
+ * @returns {string} The formatted dollar amount.
+ */
+export function dollar_format(x) {
+  const formatter = format("$,.0f");
+  return formatter(x);
+}
