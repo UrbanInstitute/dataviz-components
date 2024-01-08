2c298fe64b0bf26c14f5e6680fed5335b81554d7
Ben Kates <benekates@gmail.com>
2024-01-08 14:43:10 -0500

    add functions to index.js

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,2 +1,4 @@
-export { default as urbanColors } from "./urbanColors.js";
+export { default as dollarFormat } from "./dollarFormat.js";
+export { default as kebabCase } from "./kebabCase.js";
 export { default as tpcColors } from "./tpcColors.js";
+export { default as urbanColors } from "./urbanColors.js";
