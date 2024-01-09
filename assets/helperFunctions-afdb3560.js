99f84500de4ff01fc734f71e69019b7c97c8e8ce
mitchthorson <mitchell@mitchellthorson.com>
2024-01-09 18:42:48 +0000

    Deploying to gh-pages from @ UrbanInstitute/dataviz-components@b3ce55a4ea454e67e92c5dbc337e8ab5cc548a1d 🚀

---
+++ assets/helperFunctions-afdb3560.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-eef63e3d.js";import{M as s}from"./index-a62a292d.js";import{u as o}from"./index-0305fac3.js";import"./iframe-cc1cbdd4.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-11d98b33.js";import"./index-a04a29e8.js";import"./index-356e4a49.js";function t(r){const n=Object.assign({h1:"h1",p:"p",pre:"pre",code:"code",h2:"h2"},o(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"utils/helperFunctions"}),`
+`,e.jsx(n.h1,{id:"helper-functions",children:"Helper Functions"}),`
+`,e.jsx(n.p,{children:"The helper functions are provided to save time during development. They can be for string/number formatting and more."}),`
+`,e.jsx(n.p,{children:"To use these, utilities, import the module like so:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`import { kebabCase, dollarFormat } from "@urbaninstitute/dataviz-components/utils";
+`})}),`
+`,e.jsx(n.h2,{id:"kebabcase",children:"kebabCase()"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`kebabCase("Hello World"); // hello-world
+`})}),`
+`,e.jsx(n.h2,{id:"dollarformat",children:"dollarFormat()"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`dollarFormat(1000000); // $1,000,000
+`})})]})}function j(r={}){const{wrapper:n}=Object.assign({},o(),r.components);return n?e.jsx(n,Object.assign({},r,{children:e.jsx(t,r)})):t(r)}export{j as default};
