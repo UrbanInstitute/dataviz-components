ab2cfb77e09cc28876b698cfaca757bd9923d3c3
Ben Kates <benekates@gmail.com>
2024-01-09 12:51:56 -0500

    put utils tests in their own folder

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,7 @@
 import { describe, it, expect } from "vitest";
-import { dollarFormat, kebabCase } from "$lib/utils/index.js";
 
 describe("sum test", () => {
   it("adds 1 + 2 to equal 3", () => {
     expect(1 + 2).toBe(3);
-  });
-});
-
-describe("kebab-case test", () => {
-  it("converts a string to kebab-case", () => {
-    expect(kebabCase("Hello World")).toBe("hello-world");
-  });
-});
-
-describe("dollar format test", () => {
-  it("converts a number to a dollar-formatted string", () => {
-    expect(dollarFormat(1000000)).toBe("$1,000,000");
   });
 });
