8bd248f749ffea2bf4bd832dd85982b017c2b745
Ben Kates <benekates@gmail.com>
2024-01-09 12:47:06 -0500

    init return top

---
+++ src/lib/ReturnTop/ReturnTop.stories.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+  import ReturnTop from "./ReturnTop.svelte";
+
+  export const meta = {
+    title: "Components/ReturnTop",
+    description: "A button for moving the browser to a specified element ID",
+    component: ReturnTop,
+    tags: ["autodocs"]
+  };
+</script>
+
+<script>
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <ReturnTop args />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    id: "target-element"
+  }}
+/>
---
+++ src/lib/ReturnTop/ReturnTop.svelte
@@ -0,0 +1,41 @@
+<script>
+  import arrow from "./arrow.svg";
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  import "../style/app.css";
+
+  /**
+   * Element ID to scroll to
+   * @type {string}
+   */
+  export let elementId = "";
+</script>
+
+<div
+  class="return-top"
+  role="button"
+  tabindex="0"
+  on:click={(e) => {
+    dispatch("click", e);
+    document.getElementById(elementId)?.scrollIntoView();
+  }}
+  on:keypress={(e) => {
+    dispatch("keypress", e);
+    document.getElementById(elementId)?.scrollIntoView();
+  }}
+>
+  <span>Return to top</span>
+  <img src={arrow} alt="arrow" />
+</div>
+
+<style>
+  .return-top {
+    display: inline-flex;
+    column-gap: 0.375rem;
+    align-items: center;
+    padding-top: var(--spacing-4);
+    font-weight: var(--font-weight-bold);
+    text-transform: uppercase;
+    cursor: pointer;
+  }
+</style>
