ff9dfae4b7f230a5412dcbf520ad020cc9eb4bfe
Ben Kates <benekates@gmail.com>
2024-01-09 13:14:13 -0500

    inline arrow svg

--- src/lib/ReturnTop/ReturnTop.svelte
+++ src/lib/ReturnTop/ReturnTop.svelte
@@ -1,8 +1,13 @@
 <script>
-  import arrow from "./arrow.svg";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   import "../style/app.css";
+  const arrow = `<svg width="30" height="30" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+<rect width="40" height="40" fill="#fff"/>
+<circle cx="20" cy="20" r="17"/>
+<path d="m20.264 8.4834 13.052 12.918h-26.103l13.051-12.918z" fill="#fff"/>
+<rect x="15.241" y="15.659" width="10.047" height="14.353" fill="#fff"/>
+</svg>`;
 
   /**
    * Element ID to scroll to
@@ -25,7 +30,7 @@
   }}
 >
   <span>Return to top</span>
-  <img src={arrow} alt="arrow" />
+  {@html arrow}
 </div>
 
 <style>
