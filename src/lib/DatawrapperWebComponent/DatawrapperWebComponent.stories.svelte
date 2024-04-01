66cf32f2bff62777449236a5c30159441bd19700
Ben Kates <benekates@gmail.com>
2024-04-01 14:29:09 -0400

    init commit

---
+++ src/lib/DatawrapperWebComponent/DatawrapperWebComponent.stories.svelte
@@ -0,0 +1,49 @@
+<script context="module">
+  import DatawrapperWebComponent from "./DatawrapperWebComponent.svelte";
+
+  export const meta = {
+    title: "Components/DatawrapperWebComponent",
+    description: "Datawrapper web component",
+    component: DatawrapperWebComponent,
+    tags: ["autodocs"],
+    parameters: {
+      docs: {
+        description: {
+          component: "DW web component lorem ipsum"
+        }
+      }
+    }
+  };
+</script>
+
+<script>
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  $: test = "Hover over a region to change this text";
+
+  let datawrapperIdReactive = "RMnkX";
+</script>
+
+<Template let:args>
+  <DatawrapperWebComponent
+    {...args}
+    on:regionclick
+    on:regionmouseenter
+    on:regionmouseleave
+    on:regionclick={(e) => (test = e.detail.data.fips)}
+    on:regionmouseenter={(e) => (test = e.detail.data.fips)}
+    on:regionmouseleave={() => (test = "Hover over a region to change this text")}
+  />
+  <p>{test}</p>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    datawrapperId: "RMnkX"
+  }}
+/>
+
+<Story name="Switcher">
+  <button on:click={() => (datawrapperIdReactive = "OnyTY")}>Change chart</button>
+  <DatawrapperWebComponent datawrapperId={datawrapperIdReactive} />
+</Story>
---
+++ src/lib/DatawrapperWebComponent/DatawrapperWebComponent.svelte
@@ -0,0 +1,31 @@
+<script>
+  // import datawrapper from "../DatawrapperIframe/events";
+  // import datawrapperEventList from "../DatawrapperIframe/datawrapper-event-list.json";
+  // import { createEventDispatcher } from "svelte";
+
+  /*** @type {string} Datawrapper chart ID */
+  export let datawrapperId;
+
+  // const dispatch = createEventDispatcher();
+
+  // datawrapperEventList.forEach((eventName) => {
+  //   if (typeof window.datawrapper !== "undefined") {
+  //     datawrapper.on(eventName, (e) => {
+  //       dispatch(eventName.replace(".", ""), e);
+  //     });
+  //   }
+  // });
+</script>
+
+<div style="width:100%; min-height:300px;">
+  <script
+    type="text/javascript"
+    defer
+    src="https://datawrapper.dwcdn.net/{datawrapperId}/embed.js"
+    charset="utf-8"
+  >
+  </script>
+  <noscript>
+    <img src="https://datawrapper.dwcdn.net/{datawrapperId}/full.png" alt="[aria-description]" />
+  </noscript>
+</div>
