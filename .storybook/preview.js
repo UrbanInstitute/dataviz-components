a6e6ed2da96863b49f363023b755f21420d6445d
mitchthorson <mitchell@mitchellthorson.com>
2024-04-03 16:00:34 -0400

    Upgrade to Storybook 8

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,7 +4,6 @@ import "$lib/style/main.css";
 /** @type { import('@storybook/svelte').Preview } */
 const preview = {
 	parameters: {
-		actions: { argTypesRegex: "^on[A-Z].*" },
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
