76749c709568732ca12fdd714eaae9dbe2217c18
Ben Kates <benekates@gmail.com>
2024-04-03 11:48:30 -0400

    init commit

---
+++ src/lib/LogoUrbanWide/LogoUrbanWide.stories.svelte
@@ -0,0 +1,45 @@
+<script context="module">
+  import Logo from "./LogoUrbanWide.svelte";
+
+  export const meta = {
+    title: "Components/LogoUrbanWide",
+    description: "An Urban Institute wide logo",
+    component: Logo,
+    tags: ["autodocs"],
+    argTypes: {
+      variant: {
+        options: ["color", "white"],
+        control: "select"
+      }
+    },
+    parameters: {
+      backgrounds: {
+        default: "light",
+        values: [
+          { name: "light", value: "#ffffff" },
+          { name: "dark", value: "#0A4C6A" }
+        ]
+      },
+      docs: {
+        description: {
+          component: "Urban Institute logo in wide format."
+        }
+      }
+    }
+  };
+</script>
+
+<script>
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <Logo {...args} />
+</Template>
+
+<Story name="Default" />
+<Story
+  name="Variant - white"
+  args={{ variant: "white" }}
+  parameters={{ backgrounds: { default: "dark" } }}
+/>
