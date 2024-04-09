d9a86249da34e81ed186e89e2c42534f70b5f410
Ben Kates <benekates@gmail.com>
2024-04-09 19:12:55 -0400

    init custom story

---
+++ src/lib/TestStory/Test.stories.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  import DatawrapperIframe from "../DatawrapperIframe/DatawrapperIframe.svelte";
+  import Button from "../Button/Button.svelte";
+  export const meta = {
+    title: "TestStory",
+    tags: ["autodocs"],
+    description: "Sit laborum nisi sunt."
+  };
+</script>
+
+<script>
+  import { Story } from "@storybook/addon-svelte-csf";
+  import { CodeOrSourceMdx } from "@storybook/blocks";
+</script>
+
+<Story name="Default">
+  <DatawrapperIframe datawrapperId="WAssZ" title="test" />
+
+  <Button>Click me</Button>
+</Story>
+<!-- <CodeOrSourceMdx>Test</CodeOrSourceMdx> -->
