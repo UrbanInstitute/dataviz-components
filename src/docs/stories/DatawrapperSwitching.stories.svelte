681bb6940f58d05265c5fd039dcee066d2995007
Ben Kates <benekates@gmail.com>
2024-04-10 14:51:50 -0400

    add DW switching examples

--- src/docs/stories/DatawrapperSwitching.stories.svelte
+++ src/docs/stories/DatawrapperSwitching.stories.svelte
@@ -1,24 +1,43 @@
 <script context="module">
   export const meta = {
     title: "Examples/Stories/Datawrapper Switching",
-    description: "Examples of switching between Datawrapper Iframes"
+    description: "Examples of switching between Datawrapper iframes"
   };
 </script>
 
 <script>
   import { Story } from "@storybook/addon-svelte-csf";
   import DatawrapperIframe from "$lib/DatawrapperIframe/DatawrapperIframe.svelte";
+  import BasicDropdown from "$lib/BasicDropdown/BasicDropdown.svelte";
   import Button from "$lib/Button/Button.svelte";
+
+  let selectedChart = "AHpJL";
+
+  const dropdownData = [
+    { value: "AHpJL", label: "Chart #1" },
+    { value: "91Q0t", label: "Chart #2" },
+    { value: "eaD2D", label: "Chart #3" }
+  ];
 </script>
 
 <Story name="Dropdown">
-  <DatawrapperIframe datawrapperId="RMnkX" title="test" />
-
-  <Button>Click me</Button>
+  <BasicDropdown
+    id="dropdown"
+    data={dropdownData}
+    inlineLabel="Dropdown"
+    bind:value={selectedChart}
+    placeholder={null}
+  />
+  {#key selectedChart}
+    <DatawrapperIframe datawrapperId={selectedChart} />
+  {/key}
 </Story>
 
 <Story name="Buttons">
-  <DatawrapperIframe datawrapperId={"4BsZm"} title="another test" />
-
-  <Button>Click me</Button>
+  <Button on:click={() => (selectedChart = "AHpJL")}>Chart #1</Button>
+  <Button on:click={() => (selectedChart = "91Q0t")}>Chart #2</Button>
+  <Button on:click={() => (selectedChart = "eaD2D")}>Chart #3</Button>
+  {#key selectedChart}
+    <DatawrapperIframe datawrapperId={selectedChart} />
+  {/key}
 </Story>
