373b2df3bf84c3a1a61b27863470a7953867c5b9
mitchthorson <mitchell@mitchellthorson.com>
2024-04-10 20:09:17 -0400

    Reorg basicpage example

--- src/docs/stories/BasicPage.stories.svelte
+++ src/docs/stories/BasicPage.stories.svelte
@@ -21,7 +21,35 @@
 <Template let:args>
 </Template>
 
-<Story name="BasicPage" args={{}} >
+<Story name="BasicPage" args={{}} source={`
+<script>
+  import { Navbar, Block, ChartBlock, DatawrapperIframe, TextBlock, SocialShare, ProjectCredits } from "@urbaninstitute/dataviz-components";
+</script>
+<Navbar sticky={true}/>
+<Block>
+  <h1 style="text-align: center;">Basic Page example headline</h1>
+  <SocialShare />
+</Block>
+<div style="margin-bottom: var(--spacing-12);"></div>
+<TextBlock>
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies sit amet ex eget accumsan. Morbi vel libero dolor. Phasellus ipsum leo, rhoncus sed ipsum quis, finibus porttitor sem. Vestibulum luctus turpis sed nibh porttitor, id malesuada ex fringilla. Sed ac nisl porta, congue lacus non, convallis metus. In a dolor efficitur, scelerisque felis vitae, vulputate libero. Mauris tristique purus lectus, ut convallis libero volutpat ac. In nec scelerisque ipsum. Sed luctus lacus nibh, nec rutrum erat faucibus consectetur. Nullam tempor condimentum leo et consequat. Ut finibus arcu ex, quis congue risus imperdiet vitae. Sed rhoncus, lectus vitae gravida faucibus, urna leo dictum neque, eu semper dolor dolor ut leo. Donec id malesuada nisi. Aliquam lobortis egestas sollicitudin. Nullam ut tempus nisl.
+</TextBlock>
+<ChartBlock title="Example Chart" source="Chart source example" notes="Chart notes example">
+  <DatawrapperIframe datawrapperId="xff5j" />
+</ChartBlock>
+<div style="margin-bottom: var(--spacing-8);"></div>
+<TextBlock>
+Vestibulum quis eros ipsum. In finibus risus nec felis tincidunt placerat. Fusce eu egestas tortor, ac porttitor tortor. Vestibulum vestibulum, diam vitae cursus ullamcorper, enim risus luctus purus, id porttitor ex quam imperdiet arcu. Nunc mollis, elit ut accumsan dictum, est enim facilisis eros, in malesuada quam augue vitae nibh. Duis sed mi ut nisi facilisis fringilla a eget purus. Nunc finibus velit augue, et convallis sapien ultricies sit amet.
+
+</TextBlock>
+<TextBlock>
+Pellentesque hendrerit libero vitae mi sollicitudin cursus. Aenean congue ac sem in auctor. Donec vulputate quam quis lacus aliquet gravida. Phasellus fermentum bibendum est ac egestas. Cras fringilla, orci ac pellentesque lobortis, elit nisl dignissim nibh, id rhoncus mauris purus et mauris. Ut at tincidunt turpis. In sagittis ultricies tellus at congue. Nullam a dapibus nisl. Curabitur consectetur nisl maximus, sollicitudin lacus dapibus, vulputate purus. Nulla vel sagittis velit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer semper sapien libero, at pulvinar nibh gravida non. Etiam laoreet dignissim posuere. Nulla sed placerat ligula. 
+</TextBlock>
+<ProjectCredits heading="Credits" items={[{label: "Credits", content: "Name Name"}, {label: "Credits", content: "Name Name"}, {label: "Credits", content: "Name Name"}]}>
+<h4>About the data</h4>
+<TextBlock slot="intro"><em>Example credits data text.</em></TextBlock>
+</ProjectCredits>
+`} >
   <Navbar sticky={true}/>
   <Block>
     <h1 style="text-align: center;">Basic Page example headline</h1>
@@ -31,7 +59,7 @@
   <TextBlock>
    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies sit amet ex eget accumsan. Morbi vel libero dolor. Phasellus ipsum leo, rhoncus sed ipsum quis, finibus porttitor sem. Vestibulum luctus turpis sed nibh porttitor, id malesuada ex fringilla. Sed ac nisl porta, congue lacus non, convallis metus. In a dolor efficitur, scelerisque felis vitae, vulputate libero. Mauris tristique purus lectus, ut convallis libero volutpat ac. In nec scelerisque ipsum. Sed luctus lacus nibh, nec rutrum erat faucibus consectetur. Nullam tempor condimentum leo et consequat. Ut finibus arcu ex, quis congue risus imperdiet vitae. Sed rhoncus, lectus vitae gravida faucibus, urna leo dictum neque, eu semper dolor dolor ut leo. Donec id malesuada nisi. Aliquam lobortis egestas sollicitudin. Nullam ut tempus nisl.
   </TextBlock>
-  <ChartBlock title="Example Chart" source="Chart source example" notes="Chart notes example">
+  <ChartBlock title="Medical Debt in Colorado Is Concentrated in Immigrant-Dense Communities" source="February 2022 Urban Institute credit bureau data and the 2018–22 American Community Survey." notes="Each bubble represents a zip code in Colorado. The percentage with medical debt in collections is the share of adults in the zip code with medical debt in collections in their credit file. Percentage not citizen is the share of the population in the zip code who are not American citizens. Immigrant-dense communities are zip codes where noncitizens make up more than 15 percent of the population. The straight black line is a linear best fit. The size of the bubble is proportional to the number of consumers with a credit report in our credit bureau data. We only report statistics for zip codes with more than 50 consumers.">
     <DatawrapperIframe datawrapperId="xff5j" />
   </ChartBlock>
   <div style="margin-bottom: var(--spacing-8);"></div>
