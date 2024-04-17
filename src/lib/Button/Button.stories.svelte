4ee0dd7302f0063fa51ae40df3662ce05f68766f
Ben Kates <benekates@gmail.com>
2024-04-17 11:29:28 -0400

    add back camel case for button story testing

--- src/lib/Button/Button.stories.svelte
+++ src/lib/Button/Button.stories.svelte
@@ -43,19 +43,19 @@
 <Story
   name="primary with event listeners"
   args={{
-    eventClick: fn(),
-    eventMouseEnter: fn(),
-    eventMouseLeave: fn()
+    event_click: fn(),
+    event_mouseenter: fn(),
+    event_mouseleave: fn()
   }}
   play={async ({ canvasElement, args }) => {
     const canvas = within(canvasElement);
     const button = canvas.getByRole("button");
     await userEvent.click(button);
-    await expect(args.eventClick).toHaveBeenCalled();
+    await expect(args.event_click).toHaveBeenCalled();
     await userEvent.hover(button);
-    await expect(args.eventMouseEnter).toHaveBeenCalled();
+    await expect(args.event_mouseenter).toHaveBeenCalled();
     await userEvent.unhover(button);
-    await expect(args.eventMouseLeave).toHaveBeenCalled();
+    await expect(args.event_mouseleave).toHaveBeenCalled();
   }}
 />
 
