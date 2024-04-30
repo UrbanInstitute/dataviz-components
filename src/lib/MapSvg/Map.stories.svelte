8210adf01761bc8f5689d6c35397af9c126fdc2a
mitchthorson <mitchell@mitchellthorson.com>
2024-04-29 21:26:25 -0400

    Get map component working

---
+++ src/lib/MapSvg/Map.stories.svelte
@@ -0,0 +1,37 @@
+<script context="module">
+  import Map from "./Map.svelte";
+  import PolygonLayer from "./PolygonLayer.svelte";
+
+  export const meta = {
+    title: "Components/Map",
+    description: "An SVG map",
+    component: Map,
+    tags: ["autodocs"],
+    argTypes: {
+      arrowFillColor: { control: "color" },
+      data: { control: "object" }
+    },
+    parameters: {
+      docs: {
+        description: {
+          component: "SVG Map component"
+        }
+      }
+    }
+  };
+</script>
+<script>
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import states from "../../docs/sample-data/cb_2023_us_state.geojson";
+  console.log(states);
+</script>
+
+<Template let:args>
+  <Map {...args}>
+    <PolygonLayer />
+  </Map>
+</Template>
+
+<Story
+  name="Default"
+/>
---
+++ src/lib/MapSvg/Map.svelte
@@ -0,0 +1,21 @@
+<script>
+import { LayerCake, Svg, Canvas, Html } from 'layercake';
+import { geoAlbersUsa } from 'd3-geo';
+import states from "../../docs/sample-data/cb_2023_us_state.geojson";
+console.log(states);
+
+export let geojson = states;
+const flatData = geojson.features.map(d => d.properties);
+</script>
+<p>Map component</p>
+<div class="chart-container">
+  <LayerCake
+    {data}
+    {flatData}
+  >
+    <Svg>
+      <slot />
+    </Svg>
+  </LayerCake>
+</div>
+
