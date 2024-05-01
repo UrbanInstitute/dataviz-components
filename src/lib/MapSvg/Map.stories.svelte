1527454546505efdcfb0c65390c44674ea97c9c0
mitchthorson <mitchell@mitchellthorson.com>
2024-05-01 10:32:12 -0400

    WIP more maps

--- src/lib/MapSvg/Map.stories.svelte
+++ src/lib/MapSvg/Map.stories.svelte
@@ -22,30 +22,38 @@
     }
   };
 </script>
+
 <script>
   import { Story, Template } from "@storybook/addon-svelte-csf";
   import states from "../../docs/sample-data/states_geo.json";
-  import {urbanColors} from "$lib/utils";
+  import nyc_income from "../../docs/sample-data/nyc_income_geo.json";
+  import { urbanColors } from "$lib/utils";
+  import { geoIdentity } from "d3-geo";
+  import { scaleQuantize } from "d3-scale";
+  import { extent, max } from "d3-array";
+
+  console.log(nyc_income);
 
   const highlightStates = ["Maryland", "New York", "Pennsylvania", "Ohio"];
 
-  const highlightFeatures = states.features.filter((d) => highlightStates.includes(d.properties.NAME))
+  const highlightFeatures = states.features.filter((d) =>
+    highlightStates.includes(d.properties.NAME)
+  );
 
   function getColor(feature) {
     if (highlightStates.includes(feature.properties.NAME)) {
       return urbanColors.blue;
     }
-    return urbanColors.blue_shade_lightest;
+    return urbanColors.gray_shade_lighter;
   }
 
+  let incomeScale = scaleQuantize().domain(extent(nyc_income.features, d => d.properties.estimate)).range(urbanColors.getMapBlues());
+
+  console.log(incomeScale.range())
 </script>
 
 <Template let:args>
-  <Map {...args}>
-    <PolygonLayer fill={getColor} />
-    <PointLayer features={highlightFeatures} fill={urbanColors.blue_shade_darker}/>
-    <LabelLayer features={highlightFeatures} getLabel={(feature) => feature.properties.NAME}/>
-  </Map>
+  <Map {...args}></Map>
 </Template>
 
 <Story
@@ -53,4 +61,18 @@
   args={{
     features: states.features
   }}
-/>
+>
+  <Map features={states.features}>
+    <PolygonLayer fill={getColor} />
+    <PointLayer features={highlightFeatures} fill={urbanColors.blue_shade_darker} />
+    <LabelLayer features={highlightFeatures} getLabel={(feature) => feature.properties.NAME} />
+  </Map>
+</Story>
+
+<Story
+  name="NYC Income"
+>
+  <Map projection={geoIdentity} features={nyc_income.features}>
+    <PolygonLayer reflectY fill={(d) => incomeScale(d.properties.estimate)} />
+  </Map>
+</Story>
---
+++ src/lib/MapSvg/PolygonLayer.stories.svelte
@@ -0,0 +1,50 @@
+<script context="module">
+  import Map from "./Map.svelte";
+  import PolygonLayer from "./PolygonLayer.svelte";
+
+  export const meta = {
+    title: "Components/SVGMap/PolygonLayer",
+    description: "A polygon layer for use in a Map component.",
+    component: PolygonLayer,
+    tags: ["autodocs"],
+    argTypes: {
+      geojson: { control: "array" }
+    },
+    parameters: {
+      docs: {
+        description: {
+          component: "A polygon layer for use in a Map component."
+        }
+      }
+    }
+  };
+</script>
+<script>
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import states from "../../docs/sample-data/states_geo.json";
+  import {urbanColors} from "$lib/utils";
+
+  const highlightStates = ["Maryland", "New York", "Pennsylvania", "Ohio"];
+
+
+  function getColor(feature) {
+    if (highlightStates.includes(feature.properties.NAME)) {
+      return urbanColors.blue;
+    }
+    return urbanColors.blue_shade_lightest;
+  }
+
+</script>
+
+<Template let:args>
+  <Map>
+    <PolygonLayer {...args} fill={getColor}/>
+  </Map>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    geojson: states
+  }}
+/>
