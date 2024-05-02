5acac51c4fd4d02307cbe6caeb77cb7a62527dba
mitchthorson <mitchell@mitchellthorson.com>
2024-05-01 22:10:27 -0400

    Better documentation and options for map layers

--- src/lib/MapSvg/Map.svelte
+++ src/lib/MapSvg/Map.svelte
@@ -1,26 +1,32 @@
 <script>
   import { LayerCake, Svg } from "layercake";
-  import { writable, readable } from "svelte/store";
+  import { readable, writable } from "svelte/store";
   import { setContext } from "svelte";
   import { geoAlbersUsa } from "d3-geo";
 
-  export let features = [];
-  export let data = [];
+  /*
+   * An array of geojson features to be displayed on the map. The map will scale the projection to fit this set of features.
+   */
+  export let features;
   export let projection = geoAlbersUsa;
 
   // create stores of map global settings to add to context
   $: projectionStore = readable(projection);
   $: featuresStore = readable(features);
+  $: widthStore = writable(500);
+  $: heightStore = writable(300);
 
   // add global stores to context
   $: setContext("map", {
     projection: projectionStore,
-    features: featuresStore
+    features: featuresStore,
+    width: widthStore,
+    height: heightStore
   });
 </script>
 
-<div class="chart-container">
-  <LayerCake {data} >
+<div class="chart-container" bind:clientWidth={$widthStore} bind:clientHeight={$heightStore}>
+  <LayerCake >
     <Svg>
       <slot />
     </Svg>
