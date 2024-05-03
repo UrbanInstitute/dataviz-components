b52312065d00686d2699d338f9bf456e929c3828
mitchthorson <mitchell@mitchellthorson.com>
2024-05-03 16:36:40 -0400

    Add minzoom to labellayer

--- src/lib/SVGMap/PolygonLayer.stories.svelte
+++ src/lib/SVGMap/PolygonLayer.stories.svelte
@@ -23,6 +23,7 @@
 </script>
 
 <script>
+  import { within, userEvent, expect, fn } from "@storybook/test";
   import { Story, Template } from "@storybook/addon-svelte-csf";
   import states from "../../docs/sample-data/states_geo.json";
   import { urbanColors } from "$lib/utils";
@@ -30,13 +31,23 @@
 
 <Template let:args>
   <SVGMap features={args.features}>
-    <PolygonLayer {...args} />
+    <PolygonLayer {...args} on:mouseout on:mousemove/>
   </SVGMap>
 </Template>
 
 <Story
   name="Default"
   args={{
+    features: states.features,
+    fill: urbanColors.blue
+  }}
+/>
+
+<Story
+  name="With event listeners"
+  args={{
+    event_mousemove: fn(),
+    event_mouseout: fn(),
     features: states.features,
     fill: urbanColors.blue
   }}
