55ebbc1a91e6e913bae43c0f7f93bc9fdbdac326
mitchthorson <mitchell@mitchellthorson.com>
2024-05-08 15:37:08 -0400

    Add type definitions to helper functions

--- src/lib/SVGMap/lib.js
+++ src/lib/SVGMap/lib.js
@@ -1,3 +1,10 @@
+/*
+ * Computes a fill color with a string or function and a fallback NA color
+ * @param { Object | null } feature The data object from which to compute a dynamic color
+ * @param { (Object) => string | string } fill The function used to compute the color string or a static color
+ * @param { string } naFill The fallback color if the feature doesn't return a color from the fill function
+ * @returns { string }
+ */
 export function getFill(feature, fill, naFill) {
   if (typeof fill === "string") {
     return fill;
@@ -5,6 +12,12 @@ export function getFill(feature, fill, naFill) {
   return fill(feature) || naFill;
 }
 
+/*
+ * Computes the stroke based on a feature and a stroke function or a stroke color
+ * @param { Object | null } feature The feature used to compute the color
+ * @param { (Object) => string | string } stroke The stroke color or a function that computes the stroke color
+ * @returns { string }
+ */
 export function getStroke(feature, stroke) {
   if (typeof stroke === "string") {
     return stroke;
