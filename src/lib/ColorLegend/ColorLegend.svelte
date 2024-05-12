1e7ddbcbe5e1ac9688c6f2fd0bb648e336157216
mitchthorson <mitchell@mitchellthorson.com>
2024-05-12 11:42:00 -0400

    Organize color legend code to be more sveltey

--- src/lib/ColorLegend/ColorLegend.svelte
+++ src/lib/ColorLegend/ColorLegend.svelte
@@ -5,16 +5,17 @@
 </script>
 
 <script>
-  import { onMount } from "svelte";
   import { interpolate, quantize, interpolateRound } from "d3-interpolate";
   import { scaleLinear, scaleBand } from "d3-scale";
   import { range as d3Range, quantile } from "d3-array";
   import { format } from "d3-format";
+
   /*
    * D3 scale to base legend on
    * @type { Function } [scale]
    */
   export let scale;
+
   /*
    * Height of the visual element of the legend
    * @type { Number } [height = 20]
@@ -31,154 +32,208 @@
    * Optional number of ticks to pass to d3's generator
    * @type { Number } [ticks = 3]
    */
-  export let ticks = 3;
+  export let ticks = 5;
+
+  /*
+   * Optional size for tick labels
+   * @type { Number } [ticksSize = 12]
+   */
+  export let tickSize = 12;
+
+  /*
+   * Optional size of space between color and tick labels
+   * @type { Number } [ticksMargin = 6]
+   */
+  export let tickMargin = 10;
 
   /*
    * Optional tick formatting string or function
-   * { string | (Object) => string } [tickFormat = (d) => d]
+   * { string | (Object) => string } [tickFormat = undefined]
    */
-  export let tickFormat = (d) => d;
+  export let tickFormat = undefined;
 
-  let tickFormatFn = typeof tickFormat === "string" ? format(string) : tickFormat;
-
-  $: domain = scale.domain();
-  $: range = scale.range();
+  /*
+   * Optional margin object that defines space around legend within the SVG element
+   * @type {{top: Number, right: Number, bottom: Number, left: Number}}
+   */
+  export let margin = {
+    top: 0,
+    right: 10,
+    bottom: 0,
+    left: 0
+  };
 
   // will hold width of DOM element
   let width;
 
-  // will hold scale to compute x position of labels
-  // set by `initializeScale`
-  let xScale;
+  $: domain = scale.domain();
+  $: range = scale.range();
+
+  $: legendWidth = width - margin.left - margin.right;
 
   /*
-   * will hold scale type string, set by `initializeScale`
    * @type { scaleType }
    */
-  let scaleType;
+  $: scaleType = getScaleType(scale);
+
+  // will hold scale to compute x position of labels
+  // set by `updateScales`
+  $: xScale = getXScale(scale, scaleType, legendWidth);
 
   let scaleId = uid();
+
+  // holds reference to scale thresholds when a threshold scale is used
+  let thresholds;
+
+  $: if (scaleType === "threshold") {
+    thresholds = scale.thresholds
+      ? scale.thresholds() // scaleQuantize
+      : scale.quantiles
+        ? scale.quantiles() // scaleQuantile
+        : scale.domain(); // scaleThreshold
+  }
+
+  $: tickFormatFn = getTickFormatFn(tickFormat, thresholds);
+  $: legendTicks = getTicks(scaleType, xScale, thresholds);
+
+  function getTickFormatFn(tickFormat, thresholds) {
+    // respect any user provided options first
+    if (typeof tickFormat === "string") {
+      return format(tickFormat);
+    } else if (tickFormat) {
+      return tickFormat;
+    } else if (thresholds) {
+      // if we have a threshold scale without any use-provided formatting, generate threshold ticks
+      const thresholdFormat =
+        tickFormat === undefined
+          ? (d) => d
+          : typeof tickFormat === "string"
+            ? format(tickFormat)
+            : tickFormat;
+
+      return (i) => thresholdFormat(thresholds[i], i);
+    }
+    return (d) => d;
+  }
 
   // debug
   $: console.log("scaleType", scaleType);
   $: console.log("domain", domain);
   $: console.log("range", range);
   $: console.log("xScale", xScale);
+  $: console.log("legendTicks", legendTicks);
 
-  /**
-   * Initialize the scaleType and xScale variables in order to render the legend elements
-   */
-  function initializeScale(scale) {
-    // method for determining type inspired by: https://observablehq.com/@d3/color-legend
-
-    // Continuous
-    if (scale.interpolate) {
+  function getXScale(scale, scaleType, legendWidth) {
+    if (scaleType === "continuous") {
       const n = Math.min(scale.domain().length, scale.range().length);
-      xScale = scale.copy().rangeRound(quantize(interpolate([0, width]), n));
-      return "continuous";
-    }
-    // Sequential
-    else if (scale.interpolator) {
-      xScale = Object.assign(scale.copy().interpolator(interpolateRound(0, width)), {
+      return scale.copy().rangeRound(quantize(interpolate(0, legendWidth), n));
+    } else if (scaleType === "sequential") {
+      return Object.assign(scale.copy().interpolator(interpolateRound(0, legendWidth)), {
         range() {
-          scaleType = [0, width];
+          return [0, legendWidth];
         }
       });
-      // if the scale doesn’t implement ticks, generate tick values manually
-      // https://observablehq.com/@d3/color-legend
-      if (!xScale.ticks) {
-        if (tickValues === undefined) {
-          const n = Math.round(ticks + 1);
-          tickValues = d3Range(n).map((i) => quantile(scale.domain(), i / (n - 1)));
-          console.log("tickValues", tickValues);
-        }
-      }
-
-      return "sequential";
-    }
-    // Threshold
-    else if (scale.invertExtent) {
-      xScale = scaleLinear()
+    } else if (scaleType === "threshold") {
+      return scaleLinear()
         .domain([-1, range.length - 1])
-        .rangeRound([0, width]);
-
-
-      const thresholds = scale.thresholds
-        ? scale.thresholds() // scaleQuantize
-        : scale.quantiles
-          ? scale.quantiles() // scaleQuantile
-          : scale.domain(); // scaleThreshold
-
-      // setup tickFormat to point to thresholds
-      const thresholdFormat
-        = tickFormat === undefined ? d => d
-        : typeof tickFormat === "string" ? format(tickFormat)
-        : tickFormat;
-
-      tickFormatFn = (i) => thresholdFormat(thresholds[i], i);
-      // generate tickValues
-      tickValues = d3Range(thresholds.length);
-
-      return "threshold";
+        .rangeRound([0, legendWidth]);
+    } else {
+      return scaleBand().domain(scale.domain()).rangeRound([0, legendWidth]);
     }
-    // Ordinal
-    else {
-      xScale = scaleBand().domain(scale.domain()).rangeRound([0, width]);
-      tickValues = domain;
+  }
+
+  /*
+   * @param { Object } scale
+   * @returns { scaleType }
+   */
+  function getScaleType(scale) {
+    // method for determining type inspired by: https://observablehq.com/@d3/color-legend
+    if (scale.interpolate) {
+      return "continuous";
+    } else if (scale.interpolator) {
+      return "sequential";
+    } else if (scale.invertExtent) {
+      return "threshold";
+    } else {
       return "ordinal";
     }
+  }
+
+  function getTicks(scaleType, xScale, thresholds) {
+    if (tickValues) {
+      return tickValues;
+    }
+    let xDomain = xScale.domain();
+    if (scaleType === "sequential" && !xScale.ticks) {
+      // if the scale doesn’t implement ticks, generate tick values manually
+      // https://observablehq.com/@d3/color-legend
+      const n = Math.round(ticks + 1);
+      return d3Range(n).map((i) => quantile(xDomain, i / (n - 1)));
+    } else if (scaleType === "threshold") {
+      return d3Range(thresholds.length);
+    } else if (scaleType === "ordinal") {
+      return xDomain;
+    }
+    return xScale.ticks(ticks);
   }
 
   function uid() {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
   }
-  onMount(() => {
-    scaleType = initializeScale(scale, width);
-  });
 </script>
 
-<div class="scale-wrapper" bind:clientWidth={width}>
-  <svg {width} {height}>
-    {#if scaleType === "continuous"}
-      <defs>
-        <linearGradient id={scaleId}>
+<div class="legend-wrapper" bind:clientWidth={width}>
+  {#if width}
+    <svg {width} height={height + tickSize + tickMargin + margin.top + margin.bottom}>
+      <g class="legend-inner" transform="translate({margin.left}, {margin.top})">
+        {#if scaleType === "continuous"}
+          <defs>
+            <linearGradient id={scaleId}>
+              {#each range as color, i}
+                <stop offset="{(100 / (range.length - 1)) * i}%" stop-color={color} />
+              {/each}
+            </linearGradient>
+          </defs>
+          <rect fill="url(#{scaleId})" width={legendWidth} {height}></rect>
+        {:else if scaleType === "sequential"}
+          <defs>
+            <linearGradient id={scaleId}>
+              {#each domain as stop, i}
+                <stop offset="{100 * (i / (domain.length - 1))}%" stop-color={scale(stop)} />
+              {/each}
+            </linearGradient>
+          </defs>
+          <rect fill="url(#{scaleId})" width={legendWidth} {height}></rect>
+        {:else if scaleType === "threshold"}
           {#each range as color, i}
-            <stop offset="{(100 / (range.length - 1)) * i}%" stop-color={color} />
+            <rect x={xScale(i - 1)} y="0" width={xScale(i) - xScale(i - 1)} {height} fill={color}
+            ></rect>
           {/each}
-        </linearGradient>
-      </defs>
-      <rect fill="url(#{scaleId})" {width} {height}></rect>
-    {:else if scaleType === "sequential"}
-      <defs>
-        <linearGradient id={scaleId}>
-          {#each domain as stop, i}
-            <stop offset="{100 * (i / (domain.length - 1))}%" stop-color={scale(stop)} />
+        {:else if scaleType === "ordinal"}
+          {#each domain as cat, i}
+            <rect
+              x={xScale(cat)}
+              y={0}
+              width={Math.max(0, xScale.bandwidth() - 1)}
+              {height}
+              fill={scale(cat)}
+            ></rect>
           {/each}
-        </linearGradient>
-      </defs>
-      <rect fill="url(#{scaleId})" {width} {height}></rect>
-    {:else if scaleType === "threshold"}
-      {#each range as color, i}
-        <rect x={xScale(i - 1)} y="0" width={xScale(i) - xScale(i - 1)} {height} fill={color}
-        ></rect>
-      {/each}
-    {:else if scaleType === "ordinal"}
-      {#each domain as cat, i}
-        <rect
-          x={xScale(cat)}
-          y={0}
-          width={Math.max(0, xScale.bandwidth() - 1)}
-          {height}
-          fill={scale(cat)}
-        ></rect>
-      {/each}
-    {/if}
-    {#if scaleType}
-    <g class="legend-ticks"> 
-      {#each tickValues || scale.ticks(ticks) as tick}
-        <text fill="black" y={height} x={xScale(tick)}>{tickFormatFn(tick)}</text>
-      {/each}
-    </g>
-    {/if}
-  </svg>
+        {/if}
+        {#if scaleType && width && xScale}
+          <g class="legend-ticks">
+            {#each legendTicks as tick}
+              {#if scaleType !== "ordinal"}
+                <rect x={xScale(tick)} y={0} height={height + tickMargin} width={1} fill="black"
+                ></rect>
+              {/if}
+              <text fill="black" y={height + tickSize + tickMargin} x={xScale(tick)}
+                >{tickFormatFn(tick)}</text
+              >
+            {/each}
+          </g>
+        {/if}
+      </g>
+    </svg>
+  {/if}
 </div>
