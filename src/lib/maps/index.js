ae8b7924a09b9c3e57c5c22acb560563577d841a
mitchthorson <mitchell@mitchellthorson.com>
2024-05-14 16:17:04 -0400

    Add export for ColorLegend and format code

--- src/lib/maps/index.js
+++ src/lib/maps/index.js
@@ -2,3 +2,4 @@ export { default as SVGMap } from "./SVGMap/SVGMap.svelte";
 export { default as SVGPolygonLayer } from "./SVGPolygonLayer/SVGPolygonLayer.svelte";
 export { default as SVGPointLayer } from "./SVGPointLayer/SVGPointLayer.svelte";
 export { default as SVGLabelLayer } from "./SVGLabelLayer/SVGLabelLayer.svelte";
+export { default as ColorLegend } from "./ColorLegend/ColorLegend.svelte";
