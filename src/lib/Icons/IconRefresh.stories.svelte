81710e12821c16365d19381fd09aec1a698c98d9
Rachel Marconi <dec11rll@gmail.com>
2024-08-28 16:59:49 -0400

    add a refresh icon

---
+++ src/lib/Icons/IconRefresh.stories.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  import IconRefresh from "./IconRefresh.svelte";
+
+  export const meta = {
+    title: "Icons/Icons/IconRefresh",
+    component: IconRefresh,
+    tags: ["!dev", "!autodocs"],
+    parameters: {
+      githubLink: {
+        url: "/Icons/IconRefresh.svelte"
+      }
+    }
+  };
+</script>
+
+<script>
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <IconRefresh {...args} />
+</Template>
+
+<Story name="Default" />
