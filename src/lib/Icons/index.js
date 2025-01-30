a534901644f16d10bc640c3091e47aa749ecd754
Rachel Marconi <56045588+rachelmarconi@users.noreply.github.com>
2025-01-30 15:58:37 -0500

    add export for IconRefresh
    IconRefresh was in our storybook but was not exported and thus was unusable

--- src/lib/Icons/index.js
+++ src/lib/Icons/index.js
@@ -2,3 +2,4 @@ export { default as IconClose } from "./IconClose.svelte";
 export { default as IconPlus } from "./IconPlus.svelte";
 export { default as IconMinus } from "./IconMinus.svelte";
 export { default as IconSearch } from "./IconSearch.svelte";
+export { default as IconRefresh } from "./IconRefresh.svelte";
