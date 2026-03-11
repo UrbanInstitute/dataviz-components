718df64de20e1aa90b0378a57ccf88f374f867fa
mitchthorson <mitchthorson@users.noreply.github.com>
2026-03-11 14:55:17 +0000

    Deploying to gh-pages from @ UrbanInstitute/dataviz-components@3ecb1ec8f6622ff78cb10ac3149c65094437ee16 🚀

---
+++ assets/DatawrapperSwitching.docs-BsoJBgvz.js
@@ -0,0 +1,9 @@
+import{j as t}from"./iframe-BFmjYyCP.js";import{useMDXComponents as s}from"./index-CDb_tEnZ.js";import{M as e,C as i}from"./blocks-CeMUb_aN.js";import{Dropdown as a,Buttons as p,Loading as w}from"./DatawrapperSwitching.stories-BU3Gb-2A.js";import"./create-runtime-stories-DnSfEJ6v.js";import"./LoadingWrapper-Dg9ivyZT.js";import"./style-CNURdi_Z.js";import"./attributes-q36Eg1F8.js";import"./urbanColors-BkxYcPwH.js";import"./LogoUrbanAnimated-DayiJdW_.js";import"./attributes-C5AdEZsc.js";import"./class-B9SFUanI.js";import"./DatawrapperIframe-jBo6raiE.js";import"./BasicDropdown-CuacMnub.js";import"./each-DXXm_h2k.js";import"./Button-Du0iiGg9.js";import"./this-X0dQIlNe.js";function r(o){const n={h1:"h1",h3:"h3",p:"p",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{title:"Examples/Datawrapper Switching"}),`
+`,t.jsx(n.h1,{id:"datawrapper-switching",children:"Datawrapper Switching"}),`
+`,t.jsx(n.p,{children:"It's useful to switch between Datawrapper iframes. Below, we demonstrate two use cases: switching with a dropdown and switching with buttons."}),`
+`,t.jsx(n.h3,{id:"switching-with-a-dropdown",children:"Switching with a dropdown"}),`
+`,t.jsx(i,{of:a,sourceState:"shown"}),`
+`,t.jsx(n.h3,{id:"switching-with-buttons",children:"Switching with buttons"}),`
+`,t.jsx(i,{of:p,sourceState:"shown"}),`
+`,t.jsx(n.h3,{id:"switching-with-buttons-and-loading-wrapper",children:"Switching with buttons (and loading wrapper)"}),`
+`,t.jsx(i,{of:w,sourceState:"shown"})]})}function L(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{L as default};
